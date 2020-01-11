f095766139a412b872c66f68beb5af2048e5ea9e
danksh111987 <danksh111987@gmail.com>
2020-01-11 23:11:36 +0530

    Commit 2 Explicit wait on LoginPage.

--- maven-automation-framework/src/main/java/com/orangehrm/pages/LoginPage.java
+++ maven-automation-framework/src/main/java/com/orangehrm/pages/LoginPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.orangehrm.common.BaseSelenium;
 import com.orangehrm.common.WebDriverFactory;
@@ -17,12 +19,24 @@ public class LoginPage {
 	@FindBy(id="btnLogin")
 	WebElement vBtnLogin;
 	
+	//EplicitWait
+	WebDriverWait objdrwait = new WebDriverWait(WebDriverFactory.getWebDriver(), 30);
+	
 	//Constructor
 	public LoginPage(){
 		//initialize webelements
 		PageFactory.initElements(WebDriverFactory.getWebDriver(), this);
 	}
 	
+	//explict wait to check weblements are loaded on pageload, return type loginPage
+	public LoginPage isLoginPageLoaded(){
+		//wait until this element is clickable
+		objdrwait.until(ExpectedConditions.elementToBeClickable(vBtnLogin));
+		objdrwait.until(ExpectedConditions.elementToBeClickable(vPaswd));
+		objdrwait.until(ExpectedConditions.elementToBeClickable(vUserName));
+		
+		return this;
+	}
 	
 	//Generic methods should return respective page objects.
 	public HomePageAfterLogin checkValidLogin(String user, String paswd){
